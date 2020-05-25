2999f8f117dc02dc0fd3d20609d58a00ec8c4a90
Hyukpyo Hong <hphong@kaist.ac.kr>
2020-05-25 23:06:26 +0900

    To remove undefined argument, add Delta.Y into MH.KM() function.

--- 2stepDDE_sim_ind_main_noX_all.R
+++ 2stepDDE_sim_ind_main_noX_all.R
@@ -181,7 +181,7 @@ for(rep in 2:nrepeat) {
   K.i <- KI(P = theta[rep-1,3:4], maxt = max.T);
   
   # step 7: sampling the Michaelis-Menten constant K.M
-  KM.update = MH.KM.all(theta[rep-1,2] , KM.S, rep, RR.all[,1,], X.all, b = tun.KM, pri.KM = pri.KM)
+  KM.update = MH.KM.all(theta[rep-1,2] , KM.S, rep, RR.all[,1,], X.all, b = tun.KM, pri.KM = pri.KM, Delta.Y = Delta.Y)
   theta[rep,2] = KM.update$km;
   KM.S = KM.update$s
   count_KM = count_KM + KM.update$count
